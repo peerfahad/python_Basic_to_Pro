b98c3d8f6e5688868dd04c44e2ffba2793dcfc9f
peerfahad <fahadrashid611@gmail.com>
2020-05-05 23:41:31 +0500

    first commit

---
+++ python.py
@@ -0,0 +1,27 @@
+import turtle
+
+a =5
+print(a)
+
+b = "hello"
+print(b)
+credit_card = 444662435243
+print(credit_card)
+
+# square
+qazi_turtle =  turtle.Turtle()
+
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+square()
+qazi_turtle.forward(200)
+square()
+

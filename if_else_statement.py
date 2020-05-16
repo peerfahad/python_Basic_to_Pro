2e0330f4ccf54404166e5c2a037a6203609a7f41
peerfahad <fahadrashid611@gmail.com>
2020-05-16 23:59:51 +0500

    Index Slicing

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,5 +1,26 @@
-elephant_weight = 3000
-ant_weight = 0.1
+# elephant_weight = 3000
+# ant_weight = 0.1
 
-if elephant_weight > ant_weight:
-     print("Elephant weight is greater then aunt weight")
+# if elephant_weight > ant_weight:
+#      print("Elephant weight is greater then aunt weight")  
+# else:
+#     print("elephand weight is less then aunt weight")
+
+ 
+# #  fahad = 'happy'
+# #  while fahad ==  happy:
+# #      print("Fahad is happy")
+
+# for count in range(5):
+#      print("hi")
+
+# Index Slicing
+digits = [0,1,2,3,4,5,6,7,8,9]
+
+# print(digits[0:10:3])
+
+# for i in range(len(digits)):
+#      print(digits[0:i])
+window_size = 4
+for i in range(len(digits)-(window_size-1)):
+     print(digits[i:i+ window_size])
